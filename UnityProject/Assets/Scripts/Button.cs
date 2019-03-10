05ce3347c20b91fa699e3b06d3f19771ef467763
Rehj Cantrell <rcantrel@Pillar-of-Autumn-2.local>
2019-03-10 11:44:31 -0400

    Added first draft of timeline stuff, plus some text boxes that don't work yet to display timeline info

---
+++ UnityProject/Assets/Scripts/Button.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour {
+    public TimeLine timeline;
+
+    public enum Dir {forward, neither, backward};
+    public Dir dir=Dir.neither;
+	
+	public BoxCollider box;
+
+
+    public void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Button: clicked");
+            if (clickWasHere()) {
+                Debug.Log("Button: input sensed");
+                if (timeline!=null) {
+                    if (dir==Dir.forward) {
+                        timeline.increment();
+                    } else if (dir==Dir.backward) {
+                        timeline.decrement();
+                    } else {
+                        Debug.Log("Button: no direction");
+                    }
+                } else {
+                    Debug.Log("Button: timeline was null");
+                }
+            } else {
+                Debug.Log("Button: click was not here.");
+            }
+        }
+    }
+
+    public bool clickWasHere()
+    {
+        Vector3 pz = Layout.translateToScreen(Input.mousePosition);
+        return box.bounds.Contains(pz);
+    }
+
+	void Start () {
+        Transform parent = this.transform.parent;
+        box = parent.GetComponent<BoxCollider>();
+        TimeLine[] timelines=FindObjectsOfType<TimeLine>();
+        if (timelines.Length>0) {
+            timeline=timelines[0];
+        }
+	}
+
+}
---
+++ UnityProject/Assets/Scripts/TimeLine.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLine : MonoBehaviour {
+
+    public enum Month {Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+
+    public Month month=Month.Apr;
+    public int year=1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void increment() {
+        switch (month) {
+            case Month.Jan:
+                month = Month.Feb;
+                break;
+            case Month.Feb:
+                month = Month.Mar;
+                break;
+            case Month.Mar:
+                month = Month.Apr;
+                break;
+            case Month.Apr:
+                month = Month.May;
+                break;
+            case Month.May:
+                month = Month.Jun;
+                break;
+            case Month.Jun:
+                month = Month.Jul;
+                break;
+            case Month.Jul:
+                month = Month.Aug;
+                break;
+            case Month.Aug:
+                month = Month.Sep;
+                break;
+            case Month.Sep:
+                month = Month.Oct;
+                break;
+            case Month.Oct:
+                month = Month.Nov;
+                break;
+            case Month.Nov:
+                month = Month.Dec;
+                break;
+            case Month.Dec:
+                month = Month.Jan;
+                year++;
+                break;
+        }
+    }
+
+    public void decrement()
+    {
+        switch (month)
+        {
+            case Month.Jan:
+                month=Month.Dec;
+                year--;
+                break;
+            case Month.Feb:
+                month = Month.Jan;
+                break;
+            case Month.Mar:
+                month = Month.Feb;
+                break;
+            case Month.Apr:
+                month = Month.Mar;
+                break;
+            case Month.May:
+                month = Month.Apr;
+                break;
+            case Month.Jun:
+                month = Month.May;
+                break;
+            case Month.Jul:
+                month = Month.Jun;
+                break;
+            case Month.Aug:
+                month = Month.Jul;
+                break;
+            case Month.Sep:
+                month = Month.Aug;
+                break;
+            case Month.Oct:
+                month = Month.Sep;
+                break;
+            case Month.Nov:
+                month = Month.Oct;
+                break;
+            case Month.Dec:
+                month = Month.Nov;
+                break;
+        }
+    }
+
+}
